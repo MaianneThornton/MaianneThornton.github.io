7a967c81a52ce8781957cd1b958e09069411633f
MaianneThornton <maianne.thornton@gmail.com>
2021-10-07 15:14:37 -0400

    Updated styles and functionability

--- Portfolio/Arcade/Snake/script.js
+++ Portfolio/Arcade/Snake/script.js
@@ -8,9 +8,7 @@ const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
   if (gameOver) {
-    if (confirm('You lost. Press ok to restart.')) {
-      window.location = '/'
-    }
+    alert(`You lost. It's ok. Refresh to try again.`)
     return
   }
 
