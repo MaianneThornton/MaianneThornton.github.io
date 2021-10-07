632baf8612dfd7d3e0584e91689f2419fc28e380
MaianneThornton <maianne.thornton@gmail.com>
2021-10-07 12:06:04 -0400

    added timing, update, and draw functions. Imported snake.js and food.js

---
+++ Portfolio/Arcade/Snake/script.js
@@ -0,0 +1,30 @@
+import {update as  updateSnake,draw as drawSnake, snakeSpeed} from './snake.js';
+import {update as updateFood, draw as drawFood}from './food.js';
+
+let lastRenderTime = 0;
+const gameBoard = document.getElementById('game-board')
+
+function main(currentTime){
+window.requestAnimationFrame(main);
+const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000; //convert from milliseconds
+  if (secondsSinceLastRender < 1 / snakeSpeed) return;
+
+  console.log(`Render`);
+  lastRenderTime = currentTime
+
+  update()
+  draw()
+};
+
+window.requestAnimationFrame(main);
+
+function update(){
+updateSnake()
+updateFood()
+};
+
+function draw() {
+  gameBoard.innerHTML = '' //erases extra segments
+  drawSnake(gameBoard)
+  drawFood(gameBoard)
+};
